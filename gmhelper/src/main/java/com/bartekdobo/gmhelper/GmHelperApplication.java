03781b6f7c14b6a10c5e7e073d2312c9880c35c4
Bartłomiej Dobosz <33525309+conan8@users.noreply.github.com>
2021-02-11 17:59:42 +0100

    Internationalization
    Setup internationalization, update views to use it

--- gmhelper/src/main/java/com/bartekdobo/gmhelper/GmHelperApplication.java
+++ gmhelper/src/main/java/com/bartekdobo/gmhelper/GmHelperApplication.java
@@ -1,13 +1,40 @@
 package com.bartekdobo.gmhelper;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class GmHelperApplication {
+public class GmHelperApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
 	SpringApplication.run(GmHelperApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+	SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+	return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+	LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+	localeChangeInterceptor.setParamName("lang");
+	return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+	interceptorRegistry.addInterceptor(localeChangeInterceptor());
     }
 
 }
